c4ce3f6b266823813a34526755715ea55d5b6157
sakai taigo <92532910+sakai1250@users.noreply.github.com>
2022-11-14 17:20:29 +0900

    Add files via upload

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from . import views
+
+app_name = 'users'
+urlpatterns = [
+    # デフォルトの認証URLを取り込む
+    path('',include('django.contrib.auth.urls')),
+    path('register/',views.register,name='register'),
+]
