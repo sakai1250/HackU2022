ccd54d4022a6a30874478b305db064e5019b36bd
sakai taigo <200442066@ccmailg.meijo-u.ac.jp>
2022-11-14 17:23:53 +0900

    clean file

--- HackU2022/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-app_name = 'users'
-urlpatterns = [
-    # デフォルトの認証URLを取り込む
-    path('',include('django.contrib.auth.urls')),
-    path('register/',views.register,name='register'),
-]
